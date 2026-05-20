fc669ea9a997e06ec6b5c1e0952c7b295d59e6a2
Hamsss <101792941+Hamsss@users.noreply.github.com>
2026-05-20 06:54:01 +0000

    Deploying to gh-pages from @ KAU-QuantumAILab/kau-quantumailab.github.io@f130d06f418c4e80a9c56dc0db9d846cfd70f845 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,7 +31,7 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-people",
-          title: "People",
+          title: "people",
           description: "members of the lab or group",
           section: "Navigation",
           handler: () => {
